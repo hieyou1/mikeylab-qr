17983a6c4b8d3bc6ae9c9a890855bc20700dc8b7
hieyou1 <31020711+hieyou1@users.noreply.github.com>
2024-06-27 22:28:47 -0500

    Simple dark mode

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -59,7 +59,7 @@ module.exports = {
     ],
     devServer: {
         static: {
-            directory: path.resolve(__dirname, './pub'),
+            directory: path.resolve(__dirname, '.'),
         },
         port: 3000,
         open: true,
